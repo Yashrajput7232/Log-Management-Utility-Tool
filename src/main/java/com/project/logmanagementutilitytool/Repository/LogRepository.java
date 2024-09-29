50f37cad672277c3075712b04f81e64b7a8f1a5d
Yash dilip Rajput <yash.szpuq1585@sinhgad.edu>
2024-09-29 19:19:54 +0530

    changed the Logs Repository

--- src/main/java/com/project/logmanagementutilitytool/Repository/LogRepository.java
+++ src/main/java/com/project/logmanagementutilitytool/Repository/LogRepository.java
@@ -1,20 +1,8 @@
 package com.project.logmanagementutilitytool.Repository;
-import java.util.List;
-import org.springframework.data.mongodb.repository.Query;
 
 import com.project.logmanagementutilitytool.entity.LogEntity;
-
-// import org.springframework.data.mongodb.core.aggregation.ArithmeticOperators.Log;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface LogRepository extends MongoRepository<LogEntity, String> {
-    @Query("{}")
-    List<LogEntity> findAll();
-
-    @Query("{'timestamp': {$gte: ?0, $lte: ?1}, 'message': {$regex: ?2}}")
-    List<LogEntity> findByDateAndMessage(String startDate, String endDate, String message);
-
-
-
+    // No custom queries needed here
 }
-
