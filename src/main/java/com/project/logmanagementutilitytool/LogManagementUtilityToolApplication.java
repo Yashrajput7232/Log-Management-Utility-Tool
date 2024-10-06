de59ceb1eb32a501ce52a2ef5202b00c1d0626ef
Yash dilip Rajput <yash.szpuq1585@sinhgad.edu>
2024-10-06 13:36:53 +0530

    fixed env file issue

--- src/main/java/com/project/logmanagementutilitytool/LogManagementUtilityToolApplication.java
+++ src/main/java/com/project/logmanagementutilitytool/LogManagementUtilityToolApplication.java
@@ -1,4 +1,5 @@
 package com.project.logmanagementutilitytool;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LogManagementUtilityToolApplication {
 
 	public static void main(String[] args) {
+		// Load environment variables from .env file
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(LogManagementUtilityToolApplication.class, args);
 	}
-
 }
