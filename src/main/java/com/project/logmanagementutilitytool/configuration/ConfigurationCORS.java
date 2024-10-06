b893246d8645a2bb80281481ea01ede81ed63589
Yash dilip Rajput <yash.szpuq1585@sinhgad.edu>
2024-10-06 10:45:18 +0530

    Added CORS config file

---
+++ src/main/java/com/project/logmanagementutilitytool/configuration/ConfigurationCORS.java
@@ -0,0 +1,24 @@
+package com.project.logmanagementutilitytool.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ConfigurationCORS {
+
+        @Bean
+        public WebMvcConfigurer corsConfigurer() {
+            return new WebMvcConfigurer() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/api/**")
+                            .allowedOrigins("*") // Allow all origins
+                            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                            .allowedHeaders("*"); // Allow all headers
+                }
+            };
+        }
+
+}
