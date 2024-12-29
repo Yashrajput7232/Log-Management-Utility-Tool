deaa84e057ba84285a11ce41201cc81728d28d41
Yash Rajput <yashrajput02.scoe.comp@gmail.com>
2024-12-29 15:49:39 +0530

    Cereated a main-deploy branch for render deploymnet

--- src/main/java/com/project/logmanagementutilitytool/LogManagementUtilityToolApplication.java
+++ src/main/java/com/project/logmanagementutilitytool/LogManagementUtilityToolApplication.java
@@ -13,10 +13,10 @@ public class LogManagementUtilityToolApplication {
 
 	public static void main(String[] args) {
 
-		Dotenv dotenv = Dotenv.load();
-		dotenv.entries().forEach(entry -> {
-			System.setProperty(entry.getKey(), entry.getValue());
-		});
+//		Dotenv dotenv = Dotenv.load();
+//		dotenv.entries().forEach(entry -> {
+//			System.setProperty(entry.getKey(), entry.getValue());
+//		});
 
 		SpringApplication.run(LogManagementUtilityToolApplication.class, args);
 	}
